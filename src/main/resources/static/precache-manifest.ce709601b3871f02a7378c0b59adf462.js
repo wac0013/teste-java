4a423d51b524fe04e655323c964a716b97f407f7
Wellington Alves <wac.0013@gmail.com>
2019-10-14 12:22:43 -0300

    Adicionando arquivos compilados do front-end
    Adicionando arquivos compilados do front-end

---
+++ src/main/resources/static/precache-manifest.ce709601b3871f02a7378c0b59adf462.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "e2b4f3e2d613723388ed",
+    "url": "/js/chunk-vendors.55449ec4.js"
+  },
+  {
+    "revision": "274e1e36fe1a2f43bcd3",
+    "url": "/js/app.cfbc8cee.js"
+  },
+  {
+    "revision": "3e4df8ea577e14d3e86dc495f254fa4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "e2b4f3e2d613723388ed",
+    "url": "/css/chunk-vendors.fe06bcc7.css"
+  }
+];
